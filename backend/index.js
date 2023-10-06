71f371b4c6f9a9ed0be190debd622bcc208f1449
Noa Midzic <nmidzic@yahoo.com>
2023-10-06 20:35:20 +0200

    Added SQLite, added dependencides to package.json

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs/promises");
+const sqlite3 = require("sqlite3");
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,19 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Create db
+const db = new sqlite3.Database("app_db.db");
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    password TEXT,
+  )
+`);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
