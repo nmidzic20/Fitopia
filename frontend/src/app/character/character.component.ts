d3350a2788c8b4a0c1241dc2bc5070dfa628ad35
Luka Habuš <habusluka@gmail.com>
2023-10-07 21:52:37 +0200

    Added progress bar

--- frontend/src/app/character/character.component.ts
+++ frontend/src/app/character/character.component.ts
@@ -4,15 +4,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
-  styleUrls: ['./character.component.scss']
+  styleUrls: ['./character.component.scss'],
 })
 export class CharacterComponent {
   // Define character properties (you can retrieve these from your app's data)
   character = {
     name: 'Lily',
     level: 5,
-    health: 80
+    health: 80,
     // Add more character properties here
+    progress: 100,
   };
 
   totalSteps = 10000;
